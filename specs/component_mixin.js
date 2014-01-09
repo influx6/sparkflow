3e7c4f5e4e4a40b7992ccfdf0cf7ab60aac65c11
Alexander Ewetumo <trinoxf@gmail.com>
2014-01-09 19:25:00 +0100

    fixing port and socket alias bug

--- specs/component_mixin.js
+++ specs/component_mixin.js
@@ -22,9 +22,33 @@ module.exports = function(m,s,a){
     
     m.scoped('component out port to dummy inport');
     //connect example in port to dummy in port with example 'exampleIn' alias socket
-    example.connect('out',dummy,'in','exampleOut');
+    dummy.connect('in',example,'out','exampleOut');
     
     m.obj(dummy.port('in').sockets().length).is(1);
     m.obj(dummy.port('in').totalSockets()).is(1);
-    m.obj(dummy.port('in').totalConnectedOnSocket(0)).is(1);
+
+    example.loopInternalPorts('in','out');
+
+    example.port('in').listen(null,function(n){
+      console.log('example in port pushing:',n);
+    });
+
+    example.port('out').listen(null,function(n){
+      console.log('example out port:',n);
+    });
+
+    dummy.port('in').listen(null,function(n){
+      console.log('dummy in port pushing:',n);
+    });
+
+    example.port('in').beginGroup('<number>');
+    example.port('in').data('1');
+    example.port('in').endGroup('</number>');
+
+    // m.obj(example.port('in').isDelimiterEnabledForAllSockets()).isTrue();
+    example.port('in').enableDelimiterForAllSockets('%');
+    
+    example.port('in').beginGroup('<tag>');
+    example.port('in').data('tagandDelimited');
+    example.port('in').endGroup('</tag>');
 };
